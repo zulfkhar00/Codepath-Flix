d0d1db51e2213071df43f72b7b61c2091fa1474d
Zulfkhar Maukey <maukey.zulfkhar@gmail.com>
2021-09-18 21:44:57 +0400

    Local repo -> Remote repo

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Zulfkhar Maukey on 11/9/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
